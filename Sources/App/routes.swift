fe1f75ed9e3bb90a039e4b5c822e3ae0c56a88ad
최민한 <chmh0411@gmail.com>
2022-06-17 01:42:01 +0900

    add: get time

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,10 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("time") { req -> String in
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일 HH시 mm분"
+        return dateFormatter.string(from: Date())
+    }
 }
